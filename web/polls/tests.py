58d58241d8b16449a00b0445f53d837cfbc4e353
Bogdan Shchogolev <bogdan993000@gmail.com>
2020-01-23 19:39:11 +0200

    Improve unit tests
    Optimized unit tests.
    
    Signed-off-by: Bogdan SHchogolev <bogdan993000@gmail.com>

--- web/polls/tests.py
+++ web/polls/tests.py
@@ -5,20 +5,22 @@ from polls.models import Question
 
 # models test
 class PollsTest(TestCase):
+    text_question = "only a test question"
+    text_choice = "just a test answer"
 
-    def create_question(self, text="only a test"):
-        return Question.objects.create(question_text=text, pub_date=timezone.now())
-    def create_choice(self, text, question):
-        return Choice.objects.create(choice_text=text, question=question)
+    def create_question(self):
+        return Question.objects.create(question_text=PollsTest.text_question, pub_date=timezone.now())
+    def create_choice(self, question):
+        return Choice.objects.create(choice_text=PollsTest.text_choice, question=question)
 
     def test_question_creation(self):
         q = self.create_question()
         self.assertTrue(isinstance(q, Question))
-        self.assertEqual("only a test", q.question_text)
+        self.assertEqual(PollsTest.text_question, q.question_text)
 
     def test_choice_creation(self):
         q = self.create_question()
-        a = self.create_choice("just test answer",q)
+        a = self.create_choice(q)
 
         self.assertTrue(isinstance(a, Choice))
-        self.assertEqual("just test answer", a.choice_text)
+        self.assertEqual(PollsTest.text_choice, a.choice_text)
