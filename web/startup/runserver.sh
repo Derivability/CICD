2ae5fa8e19c7b7da4ac36b5de2b09e0a926c1b9d
Bogdan Shchogolev <bogdan993000@gmail.com>
2020-01-28 23:46:54 +0200

    Create docker volume for db
    Created docker volume "pgdata" for database contents.
    
    Signed-off-by: Bogdan Shchogolev <bogdan993000@gmail.com>

--- web/startup/runserver.sh
+++ web/startup/runserver.sh
@@ -3,5 +3,14 @@
 #Migrate
 python manage.py migrate
 
+cat <<EOF | python manage.py shell
+from django.contrib.auth import get_user_model
+
+User = get_user_model()  # get the currently active user model,
+
+User.objects.filter(username='admin').exists() or \
+    User.objects.create_superuser('admin', 'admin@example.com', 'pass')
+EOF
+
 #Start server
 python manage.py runserver 0.0.0.0:8000
