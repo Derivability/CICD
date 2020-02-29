71b5243a96dd787846b513aa4ee4db6839138e51
Bogdan Shchogolev <bogdan993000@gmail.com>
2020-02-29 23:31:27 +0200

    Add css styling
    Added css styling for site.
    
    Signed-off-by: Bogdan Shchogolev <bogdan993000@gmail.com>

--- web/polls/models.py
+++ web/polls/models.py
@@ -16,10 +16,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
-class MainQuestion(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-    def __str__(self):
-        return self.question_text
